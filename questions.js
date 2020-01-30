89d5ca1fbf3d297cdd63c3043bb92bc2ada8381b
PROX-E <lichlord18@gmail.com>
2020-01-29 22:22:51 -0500

    restructure questions

--- questions.js
+++ questions.js
@@ -1,31 +1,51 @@
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      question: "Commonly used data types DO NOT include:",
+      answers: {
+       a:"strings",
+       b:"booleans", 
+       c:"alerts", 
+       d:"numbers"
+      },
+      answer: "c"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      question: "The condition in an if / else statement is enclosed within ____.",
+      answers: {
+       a:"quotes", 
+       b:"curly brackets", 
+       c:"parentheses", 
+       d:"square brackets"
     },
-    
-    {
-      title: "",
-      choices: [""],
-      answer: ""
-    },
-    
-    {
-      title: "",
-      choices: [""],
-      answer: ""
+      correctAnswer: "c"
     },
     
     {
-      title: "",
-      choices: [""],
-      answer: ""
+     question: "True or false: a function must be defined before it is used in a javascript document.",
+      answers: {
+       a:"True",
+       b:"false"},
+      correctAnswer: "b"
+    },
+    
+    {
+     question: "The Css selector that targets a single specific element",
+      answers: {
+       a:"Id",
+       b:"class",
+       c:"target",
+       d:"this"},
+      correctAnswer: "a"
+    },
+    
+    {
+     question: "A div contains another nested div. The nested Div is considered the ____ element of the first div ",
+      answers: {
+       a:"Parent", 
+       b:"sibling",
+       c:"child",
+       d:"neighbor"},
+      correctAnswer: "c"
     },
     
 
